381f156bc923b1f8edf7455ed8cb52aba73193fb
sal <107442562+ch3ps@users.noreply.github.com>
2026-01-28 13:53:49 +0300

    Initial Commit

---
+++ daftar/daftarApp.swift
@@ -0,0 +1,17 @@
+//
+//  daftarApp.swift
+//  daftar
+//
+//  Created by Salem on 28/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct daftarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ daftarTests/daftarTests.swift
@@ -0,0 +1,17 @@
+//
+//  daftarTests.swift
+//  daftarTests
+//
+//  Created by Salem on 28/01/2026.
+//
+
+import Testing
+@testable import daftar
+
+struct daftarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
